8152a71a04d0bd7dc76f6a56205547b588990e01
Benjamin Pollack <benjamin@fogcreek.com>
2011-04-15 14:10:42 -0400

    make fabfile runnable by Justin if he had keys

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,15 @@
-from fabric.api import cd, env, sudo
+from fabric.api import cd, env, local, sudo
+
+_INTERNAL_REPO = 'http://our.fogbugz.com/kiln/Repo/Kiln/Experiments/ILoveMercurial'
+_EXTERNAL_REPO = 'ssh://hg@bitquabit.com/ilhg'
 
 def bqb():
     env.hosts = ['bitquabit.com']
 
 def _update_repo(repo, rev='tip'):
     with cd(repo):
+        ctx = local('hg id %s' % _INTERNAL_REPO, capture=True)
+        local('hg push -r %s %s' % (ctx, _EXTERNAL_REPO))
         sudo('hg pull', user='www-data')
         sudo('hg up -C %s' % rev, user='www-data')
 
