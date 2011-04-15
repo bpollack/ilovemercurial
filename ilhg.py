053e9c331e020f679e4cd47ac52c410ba60e253f
Benjamin Pollack <benjamin@fogcreek.com>
2011-04-15 12:35:34 -0400

    now with moderation

--- ilhg.py
+++ ilhg.py
@@ -4,7 +4,7 @@ import json
 import sqlite3
 import time
 
-from flask import Flask, g, render_template, request
+from flask import Flask, g, render_template, redirect, request, session, url_for
 app = Flask(__name__)
 
 import config
@@ -48,6 +48,7 @@ def update_tweets():
         return False
     data = json.loads(response.read())
     for t in data['results']:
+        # We don't need the real time stamp; we just need these relatively ordered
         q('insert or ignore into tweets (id, dt, user, image, text) values (?, ?, ?, ?, ?)',
                    (t['id_str'],
                     time.time(),
@@ -63,9 +64,30 @@ def update_tweets():
 def index():
     return render_template('index.html', tweets=q('select * from tweets where approved = 1 order by dt desc limit 30'))
 
-@app.route('/admin')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['logged_in'] = (request.form['magic'] == app.config['MAGIC'])
+        return redirect(url_for('admin'))
+    return '''<form action="" method=post><p>Password: <input type=password name=magic /><input type=submit value=Go /></p></form>'''
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+@app.route('/moderate', methods=['GET', 'POST'])
 def admin():
-    pass
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        for t in request.form.keys():
+            if t.startswith('tweet-'):
+                tid = t.split('-')[1]
+                status = int(request.form[t])
+                q('update tweets set approved = ? where id = ?', [status, tid])
+        c()
+    return render_template('admin.html', tweets=q('select * from tweets where approved = ? order by dt desc', [int(request.args.get('approved', 0))]))
 
 @app.route('/heartbeat')
 def heartbeat():
